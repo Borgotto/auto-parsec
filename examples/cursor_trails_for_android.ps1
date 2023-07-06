813da3c76060cf8caa55bc67d63ea8d5994007ff
Emanuele Borghini <manuborg2002@gmail.com>
2023-07-07 00:50:18 +0200

    Rename, slim down cursor_trail example

--- examples/cursor_trails_for_android.ps1
+++ examples/cursor_trails_for_android.ps1
@@ -1,4 +1,4 @@
-# This example shows how to enable Cursor Trails when a user connects and disable them when a user disconnects
+# This example enables Cursor Trails when a user connects and disable them when a user disconnects
 # This is useful for Android users who can't see the cursor when they connect to a host
 
 $LogFile = "C:\ProgramData\Parsec\log.txt"
@@ -9,21 +9,12 @@ Import-Module .\modules\cursor_trails.psm1
 # Begin monitoring the log file for changes
 # The function body is executed only when a username is detected in a new line
 Get-Content $LogFile -Wait -Tail 0 | Where-Object { $_ -match "\]\s(.+#\d+)" } | ForEach-Object {
-    $user = $Matches[1]
-
     if ($_.EndsWith(" connected.")) {
-        # Code here runs when a user connects
-
-        # This line enables Cursor Trails
+        # This line enables Cursor Trails, setting the trail length to 2
         Set-CursorTrails -Cursors 2
     }
     elseif ($_.EndsWith("disconnected.")) {
-        # Code here runs when a user disconnects
-
         # This line disables Cursor Trails
         Set-CursorTrails -Cursors 0
-    }
-    elseif ($_.EndsWith("is trying to connect to your computer.")) {
-        # Code here runs when a user attempts to connect
     }
 }
