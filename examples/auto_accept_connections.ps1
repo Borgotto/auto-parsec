4229e8529792d3ce8ac9796307c10c7c3a127a9e
Emanuele Borghini <manuborg2002@gmail.com>
2023-08-12 16:13:05 +0200

    Add filter for automatic connection acceptations

--- examples/auto_accept_connections.ps1
+++ examples/auto_accept_connections.ps1
@@ -7,13 +7,26 @@
 #
 #-----------------------------------------------------------------------------
 
+# Ban list to prevent certain users from connecting.
+#
+# Please note that this is NOT a reliable way to prevent users from connecting,
+# as they can easily get accepted by trying to connect at the same time as
+# someone else or by simply changing their username.
+$ban_file = "/path/to/ban_list.txt"
+
 # Import the key_presses module
 Import-Module $PSScriptRoot\..\modules\key_presses.psm1
 
 # Override the OnConnectAttempt function with the following:
-function OnConnectAttempt($user) {
-    Write-Host "$user is trying to connect, letting them in..."
+function OnConnect($user) {
+    # If the user is in the ban list, don't let them in
+    if (Get-Content $ban_file -ErrorAction Ignore | Select-String "\b($user)\b") {
+        Write-Warning "$user is banned, NOT letting them in..."
+        return
+    }
+
     # Auto accept all incoming connections by pressing Ctrl + F1
+    Write-Host "$user is trying to connect, letting them in..."
     Send-Keys -Keys "^({F1 3})"
 }
 
